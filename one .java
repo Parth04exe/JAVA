977d6895bf71bd331ba241ddf8ebae78f7e0c0c8
Parth04exe <mohantyparthasarathy04@gmail.com>
2024-11-16 16:51:12 +0530

    Update one .java

--- one .java
+++ one .java
@@ -1,10 +1,10 @@
-class x
+class one
 {
     public static void main(String[] args) {
         int a,b,c;
         a=4;
         b=6;
         c=a+b;
-        System.err.println(c);
+        System.out.println(c);
     }
 }
