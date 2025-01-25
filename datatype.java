72d713101d2581461c818611187fd5b5173b1708
Parth04exe <mohantyparthasarathy04@gmail.com>
2025-01-25 15:53:03 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,39 @@
+public class datatype {
+    public static void main(String args[]){
+
+    byte b=127;
+    System.out.println(b);
+    
+    short s=17;
+    System.out.println(s);
+    
+    char c='a';
+    System.out.println(c);
+    
+    char c1=97;
+    System.out.println(c1);
+    
+    char c2=65;
+    System.out.println(c2);
+
+    char c3=256;
+    System.out.println(c3);
+    
+    int a=10;
+    System.out.println(a);
+    
+    float f=3.54567890f;//f is required in float at end//7 digit after decimal
+    System.out.println(f);
+    
+    long l=1234567891234567891L;//L is required in long at end 
+    //long l=12345678912345678912L;
+    System.out.println(l);
+
+    double d=5.1234567891234567891234;//15 digit after decimal
+    System.out.println(d);
+
+    boolean bol=true;
+    //boolean bol1=True;//error no capital letter in boolean
+    System.out.println(bol);
+    }
+}
