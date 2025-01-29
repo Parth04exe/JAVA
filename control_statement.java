3ccd494ed04a977e051fa3994c89a9d572010f1b
Parth04exe <mohantyparthasarathy04@gmail.com>
2025-01-29 07:12:05 +0530

    Create control_statement.java

---
+++ control_statement.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class control_statement {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter the 1st number");
+        int n1=sc.nextInt();
+        System.out.println("Enter the 2nd number");
+        int n2=sc.nextInt();
+        
+        if(n1+n2==20)
+        {
+            System.out.println("NH4 SE");
+        }
+        else
+        {
+            System.out.println("NH2 SE");
+        }
+        System.out.println("pahuch gaya");
+
+        System.out.println("Enter the 1st number");
+        int n=sc.nextInt();
+        if(n>50)
+        {
+            System.out.println("no is greater than 50");
+            if(50%2==0)
+            {
+                System.out.println("no is divisible by 2");
+            }
+            else
+            {
+                System.out.println("no is not divisible 2");
+            }
+        }    
+        else 
+        {
+            System.out.println("no is less than 50");
+            if()
+            {
+                if()
+                {
+                }
+                else
+                {
+                }
+            }
+            else
+            {
+                if()
+                {
+                }
+                else
+                {
+                }
+            }
+        }
+    }
+}
