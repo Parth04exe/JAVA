ecb7e0854c1637e888d9a7adc9b0607de01b6ed8
Partha Sarathy Mohanty <mohantyparthasarathy04@gmail.com>
2025-02-01 06:23:12 +0530

    Create break.java

---
+++ break.java
@@ -0,0 +1,19 @@
+package Jan312025;
+
+public class Break
+{
+    public static void main(String args[])
+    {
+        for (int i = 0;i < 10;i++)
+        {
+            if (i == 5)
+            {
+                break;
+            }
+            System.out.println("i = "+i);
+
+        }
+        System.out.println("partha sarathy mohanty");
+    }
+}
+
